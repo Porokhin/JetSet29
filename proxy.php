5a887ed904c77ea47ba101548ef3058d0bbd3284
Porokhin <60346961+Porokhin@users.noreply.github.com>
2025-06-30 00:39:09 +0300

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,21 @@
+<?php
+header('Access-Control-Allow-Origin: *'); // Разрешаем запросы из Telegram Mini App
+header('Content-Type: text/html; charset=utf-8');
+
+// URL сайта Jetset29.ru (можно менять по необходимости)
+$url = 'https://jetset29.ru';
+
+// Получаем HTML сайта
+$html = file_get_contents($url);
+
+// Удаляем заголовки, которые блокируют iframe
+$html = preg_replace('/<meta[^>]+http-equiv=["\']X-Frame-Options["\'][^>]+>/i', '', $html);
+$html = preg_replace('/<meta[^>]+content-security-policy[^>]+>/i', '', $html);
+
+// Исправляем ссылки, чтобы они вели на оригинальный сайт через прокси
+$html = str_replace('href="/', 'href="' . $url . '/', $html);
+$html = str_replace('src="/', 'src="' . $url . '/', $html);
+
+// Выводим обработанный HTML
+echo $html;
+?>
